acd0ea18a6eb58e8262af40ff68b447e395f40ce
pk2011 <m.prabhakaran7@gmail.com>
2025-01-03 01:35:52 +0530

    code format

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
